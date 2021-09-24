2d4c551e4ce8d712994fac4631b27d09b52e391f
Kyrylo <krllmsk@gmail.com>
2021-09-24 10:15:19 +0300

    added contacts methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,43 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/api/contacts', async (req, res, next) => {
-  res.json({ message: 'getAllContacts' })
+const {
+  getAllContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('../../model/contacts/index')
+
+router.get('/', async (req, res, next) => {
+  const contacts = await getAllContacts()
+  res.status(200).json(contacts)
 })
 
-router.get('/api/contacts/:contactId', async (req, res, next) => {
-  res.json({ message: 'getContactById' })
+router.get('/:contactId', async (req, res, next) => {
+  const contact = await getContactById()
+  res.status(200).json(contact)
 })
 
-router.post('/api/contacts', async (req, res, next) => {
-  res.json({ message: 'addContact' })
+router.post('/', async (req, res, next) => {
+  const newContact = await addContact()
+  res.status(201).json({ message: 'Added new contact', newContact: newContact })
 })
 
-router.delete('/api/contacts/:contactId', async (req, res, next) => {
-  res.json({ message: 'removeContact' })
+router.delete('/:contactId', async (req, res, next) => {
+  // console.log(req.params.contactId)
+  const id = req.params.contactId
+
+  const result = await removeContact(id)
+  if (!result) {
+    // throw new Error(`Contact with id ${id} not found`)
+    // console.log(`Contact with id ${id} not found`)
+    res.status(404).json({ message: `Contact with id ${id} not found` })
+  }
+  res.status(200).json({ message: 'removeContact' })
 })
 
-router.patch('/api/contacts/:contactId', async (req, res, next) => {
-  res.json({ message: 'updateContact' })
+router.patch('/:contactId', async (req, res, next) => {
+  res.status(200).json({ message: 'updateContact' })
 })
 
 module.exports = router
