8723b69da7311910ec983390f04a0693b4a64338
Kyrylo Moskalenko <72569852+Kylory@users.noreply.github.com>
2021-10-12 08:55:44 +0300

    Update index.js

--- model/users/index.js
+++ model/users/index.js
@@ -2,10 +2,12 @@ const addUser = require('./addUser')
 const loginUser = require('./loginUser')
 const logoutUser = require('./logoutUser')
 const getUserByToken = require('./getUserByToken')
+const updateUserSubscription = require('./updateUserSubscription')
 
 module.exports = {
   addUser,
   loginUser,
   logoutUser,
   getUserByToken,
+  updateUserSubscription,
 }
