6fbc83d7e5f1e5181f4e206cd5b9e0c9e7b2f79d
Kyrylo <krllmsk@gmail.com>
2021-10-18 09:10:45 +0300

    added uploadConfig

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const multer = require('multer')
+const path = require('path')
 
 const contactsRouter = require('./routes/api/contacts')
 const usersRouter = require('./routes/api/users')
+
+const tempDir = path.join(__dirname, 'temp')
+
+const uploadConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir)
+  },
+  filemane: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+  limits: {
+    fileSize: 2048,
+  },
+})
+
+const upload = multer({
+  storage: uploadConfig,
+})
 
 const app = express()
 
@@ -12,6 +32,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(upload())
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', usersRouter)
