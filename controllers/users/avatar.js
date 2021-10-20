37091196b4aa1e79fd6c244a7dccd6dadb4417f7
Kyrylo Moskalenko <krllmsk@gmail.com>
2021-10-20 20:09:38 +0300

    added jimp

--- controllers/users/avatar.js
+++ controllers/users/avatar.js
@@ -1,9 +1,19 @@
 const fs = require('fs').promises
 const path = require('path')
 const { updateUserAvatar } = require('../../model/users')
+const Jimp = require('jimp')
 
 const avatar = async (req, res, next) => {
   const { path: tempDir, originalname } = req.file
+
+  await Jimp.read(tempDir)
+    .then((image) => {
+      return image.resize(250, 250).write(tempDir)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+
   const { _id } = req.user
 
   const [extension] = originalname.split(' ').reverse()
